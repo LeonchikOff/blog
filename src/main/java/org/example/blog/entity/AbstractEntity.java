615a44d0bb91b63fb9f1ef355c1fd04057be9a1f
Leon <ajnj199320@gmail.com>
2023-01-23 17:11:42 +0300

    Created of Entities and Models

---
+++ src/main/java/org/example/blog/entity/AbstractEntity.java
@@ -0,0 +1,33 @@
+package org.example.blog.entity;
+
+import org.example.blog.model.AbstractModel;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public abstract class AbstractEntity<CertainTypeOfObligatoryFieldIdWithinThisClass>
+        extends AbstractModel implements Serializable {
+
+    private CertainTypeOfObligatoryFieldIdWithinThisClass id;
+
+    public CertainTypeOfObligatoryFieldIdWithinThisClass getId() {
+        return id;
+    }
+
+    public void setId(CertainTypeOfObligatoryFieldIdWithinThisClass id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractEntity<?> that = (AbstractEntity<?>) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
